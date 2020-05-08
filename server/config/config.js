308a3640c4f9190a8555c61e64784886df1e492c
colombianet <oscar_p1986@hotmail.com>
2020-05-08 18:34:46 -0500

    variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://strider:NGCEtQgtPZId7gt6@cluster0-p9hgm.mongodb.net/test';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
 
